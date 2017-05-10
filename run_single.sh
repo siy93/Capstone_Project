2d0f64f1df049fbcca234169b0309e9ce2e46081
In Yeop Song <songin1953@gmail.com>
2017-05-10 13:00:24 +0900

    JSON 포맷 수정

--- run_single.sh
+++ run_single.sh
@@ -50,7 +50,7 @@ do
         for value in "${Naming[@]}";do
                 if [ "$code" == "$value" ];
                 then
-                        temp2[$IndenCount]="\t\t{\n\t\t\"name\":\""$code"\",\n\t\t\"row\":"$row",\n\t\t\"col\":"$col"\n\t\t},"
+                        temp2[$NamingCount]="\t\t{\n\t\t\"name\":\""$code"\",\n\t\t\"row\":"$row",\n\t\t\"col\":"$col"\n\t\t},"
                         let NamingCount=NamingCount+1;
                         let flag=1;
                         break
@@ -64,7 +64,7 @@ do
         for value in "${Comment[@]}";do
                 if [ "$code" == "$value" ];
                 then
-                        temp3[$IndenCount]="\t\t{\n\t\t\"name\":\""$code"\",\n\t\t\"row\":"$row",\n\t\t\"col\":"$col"\n\t\t},"
+                        temp3[$CommentCount]="\t\t{\n\t\t\"name\":\""$code"\",\n\t\t\"row\":"$row",\n\t\t\"col\":"$col"\n\t\t},"
                         let CommentCount=CommentCount+1;
                         let flag=1;
                         break
@@ -78,7 +78,7 @@ do
         for value in "${WhiteSpace[@]}";do
                 if [ "$code" == "$value" ];
                 then
-                        temp4[$IndenCount]="\t\t{\n\t\t\"name\":\""$code"\",\n\t\t\"row\":"$row",\n\t\t\"col\":"$col"\n\t\t},"
+                        temp4[$WhiteSpaceCount]="\t\t{\n\t\t\"name\":\""$code"\",\n\t\t\"row\":"$row",\n\t\t\"col\":"$col"\n\t\t},"
                         let WhiteSpaceCount=WhiteSpaceCount+1;
                         let flag=1;
                         break
@@ -92,7 +92,7 @@ do
         for value in "${CodeFormat[@]}";do
                 if [ "$code" == "$value" ];
                 then
-                        temp5[$IndenCount]="\t\t{\n\t\t\"name\":\""$code"\",\n\t\t\"row\":"$row",\n\t\t\"col\":"$col"\n\t\t},"
+                        temp5[$CodeFormatCount]="\t\t{\n\t\t\"name\":\""$code"\",\n\t\t\"row\":"$row",\n\t\t\"col\":"$col"\n\t\t},"
                         let CodeFormatCount=CodeFormatCount+1;
                         let flag=1;
                         break
@@ -106,7 +106,7 @@ do
         for value in "${Statement[@]}";do
                 if [ "$code" == "$value" ];
                 then
-                        temp6[$IndenCount]="\t\t{\n\t\t\"name\":\""$code"\",\n\t\t\"row\":"$row",\n\t\t\"col\":"$col"\n\t\t},"
+                        temp6[$StatementCount]="\t\t{\n\t\t\"name\":\""$code"\",\n\t\t\"row\":"$row",\n\t\t\"col\":"$col"\n\t\t},"
                         let StatementCount=StatementCount+1;
                         let flag=1;
                         break
@@ -120,7 +120,7 @@ do
         for value in "${Function[@]}";do
                 if [ "$code" == "$value" ];
                 then
-                        temp7[$IndenCount]="\t\t{\n\t\t\"name\":\""$code"\",\n\t\t\"row\":"$row",\n\t\t\"col\":"$col"\n\t\t},"
+                        temp7[$FunctionCount]="\t\t{\n\t\t\"name\":\""$code"\",\n\t\t\"row\":"$row",\n\t\t\"col\":"$col"\n\t\t},"
                         let FunctionCount=FunctionCount+1;
                         let flag=1;
                         break
@@ -134,7 +134,7 @@ do
         for value in "${Class[@]}";do
                 if [ "$code" == "$value" ];
                 then
-                        temp8[$IndenCount]="\t\t{\n\t\t\"name\":\""$code"\",\n\t\t\"row\":"$row",\n\t\t\"col\":"$col"\n\t\t},"
+                        temp8[$ClassCount]="\t\t{\n\t\t\"name\":\""$code"\",\n\t\t\"row\":"$row",\n\t\t\"col\":"$col"\n\t\t},"
                         let ClassCount=ClassCount+1;
                         let flag=1;
                         break
@@ -148,7 +148,7 @@ do
         for value in "${Module[@]}";do
                 if [ "$code" == "$value" ];
                 then
-                        temp9[$IndenCount]="\t\t{\n\t\t\"name\":\""$code"\",\n\t\t\"row\":"$row",\n\t\t\"col\":"$col"\n\t\t},"
+                        temp9[$ModuleCount]="\t\t{\n\t\t\"name\":\""$code"\",\n\t\t\"row\":"$row",\n\t\t\"col\":"$col"\n\t\t},"
                         let ModuleCount=ModuleCount+1;
                         break
                 fi
@@ -171,21 +171,21 @@ rm $FILE2
 
 
 echo -e "\"Indentation\" : {\n\t\"count\":" $IndenCount ',' >> $FILE3
-echo -e "\t\t\"error\":[\n${temp1[@]}]" >> $FILE3
+echo -e "\t\t\"error\":[\n${temp1[@]}],}," >> $FILE3
 echo -e "\"Naming\" : {\n\t\"count\":" $NamingCount ',' >> $FILE3
-echo -e "\t\t\"error\":[\n${temp2[@]}]" >> $FILE3
+echo -e "\t\t\"error\":[\n${temp2[@]}],}," >> $FILE3
 echo -e "\"Comment\" : {\n\t\"count\":" $CommentCount ',' >> $FILE3
-echo -e "\t\t\"error\":[\n${temp3[@]}]" >> $FILE3
+echo -e "\t\t\"error\":[\n${temp3[@]}],}," >> $FILE3
 echo -e "\"WhiteSpace\" : {\n\t\"count\":" $WhiteSpaceCount ',' >> $FILE3
-echo -e "\t\t\"error\":[\n${temp4[@]}]" >> $FILE3
+echo -e "\t\t\"error\":[\n${temp4[@]}],}," >> $FILE3
 echo -e "\"CodeFormat\" : {\n\t\"count\":" $CodeFormatCount ',' >> $FILE3
-echo -e "\t\t\"error\":[\n${temp5[@]}]" >> $FILE3
-echo -e "\"Statement\" : {\n\t\"count\":" $StatementCountCount ',' >> $FILE3
-echo -e "\t\t\"error\":[\n${temp6[@]}]" >> $FILE3
+echo -e "\t\t\"error\":[\n${temp5[@]}],}," >> $FILE3
+echo -e "\"Statement\" : {\n\t\"count\":" $StatementCount ',' >> $FILE3
+echo -e "\t\t\"error\":[\n${temp6[@]}],}," >> $FILE3
 echo -e "\"Function\" : {\n\t\"count\":" $FunctionCount ',' >> $FILE3
-echo -e "\t\t\"error\":[\n${temp7[@]}]" >> $FILE3
+echo -e "\t\t\"error\":[\n${temp7[@]}],}," >> $FILE3
 echo -e "\"Class\" : {\n\t\"count\":" $ClassCount ',' >> $FILE3
-echo -e "\t\t\"error\":[\n${temp8[@]}]" >> $FILE3
+echo -e "\t\t\"error\":[\n${temp8[@]}],}," >> $FILE3
 echo -e "\"Module\" : {\n\t\"count\":" $ModuleCount ',' >> $FILE3
-echo -e "\t\t\"error\":[\n${temp9[@]}]" >> $FILE3
+echo -e "\t\t\"error\":[\n${temp9[@]}],}," >> $FILE3
 echo "}" >> $FILE3
