4ef1aaed47b21f763dbbf8331ddb23f82c7f838b
In Yeop Song <songin1953@gmail.com>
2017-05-17 14:16:29 +0900

    JSON 형식 수정 & DB logic 추가

--- run_single.sh
+++ run_single.sh
@@ -1,5 +1,6 @@
 #! /bin/bash
 
+# parameter option
 if [ "$1" == "" ] || [ "$#" -gt "1" ] || [ ! -e $1 ]
 then
         echo "not exist python file or more than one file"
@@ -11,9 +12,10 @@ source error.dat
 
 mkdir -p result
 
+#result file path 
 FILE=result/temp`basename $1 .py`.txt
 FILE2=result/temp`basename $1 .py`2.txt
-FILE3=result/`basename $1 .py`.txt
+FILE3=`basename $1 .py`.json
 
 pycodestyle $1 --format='%(code)s %(row)d %(col)d' > $FILE
 pylint --msg-template='{msg_id} {line:3d} {column}' --reports=n $1 >> $FILE
@@ -169,23 +171,84 @@ done<$FILE2
 rm $FILE
 rm $FILE2
 
+#colon remove
+if [[ "$IndenCount" != "0" ]];
+then
+	temp=`echo ${temp1[$IndenCount-1]} | cut -d',' -f1`','`echo ${temp1[$IndenCount-1]} | cut -d',' -f2`','`echo ${temp1[$IndenCount-1]} | cut -d',' -f3`
+	temp1[$IndenCount-1]=$temp
+	temp=''
+fi
+if [[ "$Namingcount" != "0" ]];
+then
+	temp=`echo ${temp2[$NamingCount-1]} | cut -d',' -f1`','`echo ${temp2[$NamingCount-1]} | cut -d',' -f2`','`echo ${temp2[$NamingCount-1]} | cut -d',' -f3`
+	temp2[$NamingCount-1]=$temp
+	temp=''
+fi
+if [[ "$CommentCount" != "0" ]];
+then
+	temp=`echo ${temp3[$CommentCount-1]} | cut -d',' -f1`','`echo ${temp3[$CommentCount-1]} | cut -d',' -f2`','`echo ${temp3[$CommentCount-1]} | cut -d',' -f3`
+	temp3[$CommentCount-1]=$temp
+	temp=''
+fi
+if [[ "$WhitSpaceCount" != "0" ]];
+then
+	temp=`echo ${temp4[$WhiteSpaceCount-1]} | cut -d',' -f1`','`echo ${temp4[$WhiteSpaceCount-1]} | cut -d',' -f2`','`echo ${temp4[$WhiteSpaceCount-1]} | cut -d',' -f3`
+	temp4[$WhiteSpaceCount-1]=$temp
+	temp=''
+fi
+if [[ "$CodeFormatCount" != "0" ]];
+then
+	temp=`echo ${temp5[$CodeFormatCount-1]} | cut -d',' -f1`','`echo ${temp5[$CodeFormatCount-1]} | cut -d',' -f2`','`echo ${temp5[$CodeFormatCount-1]} | cut -d',' -f3`
+	temp5[$CodeFormatCount-1]=$temp
+	temp=''
+fi
+if [[ "$StatementCount" != "0" ]];
+then
+	temp=`echo ${temp6[$StatementCount-1]} | cut -d',' -f1`','`echo ${temp6[$StatementCount-1]} | cut -d',' -f2`','`echo ${temp6[$StatementCount-1]} | cut -d',' -f3`
+	temp6[$StatementCount-1]=$temp
+	temp=''
+fi
+if [[ "$FunctionCount" != "0" ]];
+then
+	temp=`echo ${temp7[$FunctionCount-1]} | cut -d',' -f1`','`echo ${temp7[$FunctionCount-1]} | cut -d',' -f2`','`echo ${temp7[$FunctionCount-1]} | cut -d',' -f3`
+	temp7[$FunctionCount-1]=$temp
+	temp=''
+fi
+if [[ "$ClassCount" != "0" ]];
+then
+	temp=`echo ${temp8[$ClassCount-1]} | cut -d',' -f1`','`echo ${temp8[$ClassCount-1]} | cut -d',' -f2`','`echo ${temp8[$ClassCount-1]} | cut -d',' -f3`
+	temp8[$ClassCount-1]=$temp
+	temp=''
+fi
+if [[ "$Modulecount" != "0" ]];
+then
+	temp=`echo ${temp9[$ModuleCount-1]} | cut -d',' -f1`','`echo ${temp9[$ModuleCount-1]} | cut -d',' -f2`','`echo ${temp9[$ModuleCount-1]} | cut -d',' -f3`
+	temp9[$ModuleCount-1]=$temp
+	temp=''
+fi
 
 echo -e "\"Indentation\" : {\n\t\"count\":" $IndenCount ',' >> $FILE3
-echo -e "\t\t\"error\":[\n${temp1[@]}],}," >> $FILE3
+echo -e "\t\t\"error\":[\n${temp1[@]}]}," >> $FILE3
 echo -e "\"Naming\" : {\n\t\"count\":" $NamingCount ',' >> $FILE3
-echo -e "\t\t\"error\":[\n${temp2[@]}],}," >> $FILE3
+echo -e "\t\t\"error\":[\n${temp2[@]}]}," >> $FILE3
 echo -e "\"Comment\" : {\n\t\"count\":" $CommentCount ',' >> $FILE3
-echo -e "\t\t\"error\":[\n${temp3[@]}],}," >> $FILE3
+echo -e "\t\t\"error\":[\n${temp3[@]}]}," >> $FILE3
 echo -e "\"WhiteSpace\" : {\n\t\"count\":" $WhiteSpaceCount ',' >> $FILE3
-echo -e "\t\t\"error\":[\n${temp4[@]}],}," >> $FILE3
+echo -e "\t\t\"error\":[\n${temp4[@]}]}," >> $FILE3
 echo -e "\"CodeFormat\" : {\n\t\"count\":" $CodeFormatCount ',' >> $FILE3
-echo -e "\t\t\"error\":[\n${temp5[@]}],}," >> $FILE3
+echo -e "\t\t\"error\":[\n${temp5[@]}]}," >> $FILE3
 echo -e "\"Statement\" : {\n\t\"count\":" $StatementCount ',' >> $FILE3
-echo -e "\t\t\"error\":[\n${temp6[@]}],}," >> $FILE3
+echo -e "\t\t\"error\":[\n${temp6[@]}]}," >> $FILE3
 echo -e "\"Function\" : {\n\t\"count\":" $FunctionCount ',' >> $FILE3
-echo -e "\t\t\"error\":[\n${temp7[@]}],}," >> $FILE3
+echo -e "\t\t\"error\":[\n${temp7[@]}]}," >> $FILE3
 echo -e "\"Class\" : {\n\t\"count\":" $ClassCount ',' >> $FILE3
-echo -e "\t\t\"error\":[\n${temp8[@]}],}," >> $FILE3
+echo -e "\t\t\"error\":[\n${temp8[@]}]}," >> $FILE3
 echo -e "\"Module\" : {\n\t\"count\":" $ModuleCount ',' >> $FILE3
-echo -e "\t\t\"error\":[\n${temp9[@]}],}," >> $FILE3
+echo -e "\t\t\"error\":[\n${temp9[@]}]}" >> $FILE3
 echo "}" >> $FILE3
+
+#import json to db
+#mongoimport --db test --collection docs --file $FILE3
+#rm $FILE3
+
+rm metricData.*
